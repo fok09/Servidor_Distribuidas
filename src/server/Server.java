36a195a85378fc0704b70a84cc4f0369fd0147a6
fok09 <botiukfranco@gmail.com>
2018-09-06 10:29:54 -0300

    Cambios en el server.

---
+++ src/server/Server.java
@@ -0,0 +1,32 @@
+package server;
+
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+
+import tda.TDASistemaCine;
+import app.*;
+
+public class Server {
+    
+TDASistemaCine objetoRemoto;
+	
+	public static void main(String[] args)
+	{
+		new Server();
+	}
+	
+	public Server() {
+		iniciar();
+	}
+	
+    public void iniciar() {
+    	try {
+    		LocateRegistry.createRegistry(1099);	
+            TDASistemaCine sistemaCine = new GestionAlumnos();
+            Naming.rebind ("//localhost/SistemaCine", sistemaCine);
+            System.out.println("Fijado en //localhost/SistemaCine");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
